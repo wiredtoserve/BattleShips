6108acf48263f7eaba4325570742c8d2b1be2390
mehulnagpurkar <mehulnagpurkar@MacBook-Pro-2.local>
2019-04-18 22:08:21 +1000

    Added a DummyPlayer class

--- Battleships/tasks.py
+++ Battleships/tasks.py
@@ -1,5 +1,7 @@
 # Task file that contains the classes
+# Should pass the complete class as String to main.py
 
+#import random
 
 class Player:
     """
@@ -28,6 +30,7 @@ class Player:
 
         :return: a 10x10 grid with the location of our ships
         '''
+        # Grid of rows and columns ranging from indices 0 to 9
         grid = [[0 for i in range(10)] for j in range(10)]
         for ship in range(1, 6):
             for row in range(0, ship):
@@ -55,3 +58,86 @@ class Player:
         :return: string - name of the Player
         '''
         return "Syndicate_10"
+
+
+# This part of the code is just for demonstration only
+
+class DummyPlayer:
+    """
+
+    Dummy player for test purposes
+
+    """
+    import random
+
+    def setup_ships(self):
+        '''
+
+        :return: a 10x10 grid with the location of our ships
+        '''
+        grid = [[0 for i in range(10)] for j in range(10)]
+
+        # ship numbers
+        ship = [i for i in range(1, 6)]
+
+        # randomly shuffle the ship list
+        DummyPlayer.random.shuffle(ship)
+
+        for ship_number in ship:
+            control = True
+
+            while control:
+                # randomly select a point on the grid
+                ship_start_position = self.random_position()
+
+                if self.vacant(ship_start_position, ship_number, grid):
+                    for row in range(0, ship_number):
+                        grid[ship_start_position[0] + row][ship_start_position[1]] = ship_number
+
+                    control = False
+
+        return grid
+
+    def take_turn(self, history):
+        '''
+
+        :param history: records the game
+                        list that contains one entry per turn
+                        dict with 3 keys
+                            - 'shots' : list of shots made in the turn
+                            - 'hits' : an integer number of hits that your shots made
+                            - 'incoming' : opponents list of shots, [] if moved
+
+        :return: a list of tuple/s describing the shots or changed location
+                depending on the strategy
+        '''
+        return [(0, 0), (1, 1), (2, 2)]
+
+    def get_name(self):
+        '''
+
+        :return: string - name of the Player
+        '''
+        return "Syndicate_10"
+
+    # function to select a point on the grid
+    def random_position(self):
+        return [DummyPlayer.random.randint(0, 9), DummyPlayer.random.randint(0, 9)]
+
+    # function to check if the position is vacant
+    def vacant(self, pos, ship, grid):
+        # checking if the ship will fit in the board
+        if pos[0] + ship <= 10:
+            for i in range(0, ship):
+                # checking if any other ship is already present
+                if grid[i][pos[1]] != 0:
+                    return False
+            return True
+        return False
+
+
+p1 = DummyPlayer()
+print(p1.get_name())
+grid = p1.setup_ships()
+for i in grid:
+    print(i)
